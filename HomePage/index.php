8b65bcb7be3367603f636ab1ea25ef996b3b7f4a
biu <biu@mail.ustc.edu.cn>
2019-01-18 17:06:50 +0800

    cname

--- HomePage/index.php
+++ HomePage/index.php
@@ -1,3 +1,3 @@
-<? 
-Header("Location: https://biuy.github.io"); 
-?>
+<script 
+type="text/javascript">window.location.href="https://biuy.github.io";</script>
+window.location.href="https://biuy.github.io";
